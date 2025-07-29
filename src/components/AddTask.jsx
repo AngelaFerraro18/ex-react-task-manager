8d997044dec2c8af0dde84296b0a00294e9ecb47
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-07-29 10:48:28 +0200

    feat: milestone 5 completed -> implement AddTask component with form validation and state management

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,59 @@
+import { useState, useRef } from "react";
+const symbols = "!@#$%^&*()-_=+[]{}|;:'\\,.<>?/`~";
+
 function AddTask() {
+    // input title controllato
+    const [title, setTitle] = useState('');
+
+    // textarea con useRef 
+    const descriptionRef = useRef();
+
+    // status con useRef 
+    const statusRef = useRef();
+
+    function handleForm(e) {
+        e.preventDefault();
+        if (!title) {
+            console.error('Il campo non può essere vuoto!');
+            return;
+        }
+
+        if ([...title].some(char => symbols.includes(char))) {
+            console.error('Non può contenere simboli speciali!');
+            return;
+        }
+
+        const description = descriptionRef.current.value;
+        const status = statusRef.current.value;
+
+        console.log({
+            title,
+            description,
+            status
+        })
+    }
 
     return (<>
         <h2>Aggiungi una task!</h2>
+
+        <form onSubmit={handleForm}>
+            <input type="text"
+                value={title}
+                onChange={e => setTitle(e.target.value)}
+                placeholder="Nome della task" />
+
+            <textarea
+                ref={descriptionRef}
+                placeholder="Aggiungi una descrizione..."></textarea>
+
+            <select ref={statusRef}>
+                <option value="">To do</option>
+                <option value="to-do">To Do</option>
+                <option value="doing">Doing</option>
+                <option value="done">Done</option>
+            </select>
+            <button type="submit">Aggiungi Task</button>
+        </form>
     </>)
 }
 
