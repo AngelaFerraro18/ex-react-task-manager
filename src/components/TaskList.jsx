26c5d153b0c627858964b7949d2d6e401741ca29
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-07-31 10:01:36 +0200

    feat: milestone 11 completed -> implement sorting functionality in TaskList component

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,21 +2,56 @@
 // import { GlobalContext } from "../context/GlobalContext";
 import TaskRow from "./TaskRow";
 import { useGlobalContext } from "../context/GlobalContext";
+import { useMemo, useState } from "react";
 
 function TaskList() {
 
     const { tasks } = useGlobalContext();
 
+    //variabili di stato per ordinamento
+    const [sortBy, setSortBy] = useState('CreatedAt');
+    const [sortOrder, setSortOrder] = useState(1);
+
+
+    function handleSort(column) {
+        if (sortBy === column) {
+            setSortOrder(prev => -prev);
+        } else {
+            setSortBy(column);
+            setSortOrder(1);
+        }
+    }
+
+    const sortedTasks = useMemo(() => {
+
+        const statusOrder = { 'To do': 0, 'Doing': 1, 'Done': 2 };
+
+        return [...tasks].sort((a, b) => {
+            let result = 0;
+
+            if (sortBy === 'title') {
+                result = a.title.localeCompare(b.title);
+            } else if (sortBy === 'status') {
+                result = statusOrder[a.status] - statusOrder[b.status];
+            } else if (sortBy === 'createAt') {
+                result = new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+            }
+
+            return result * sortOrder;
+        })
+
+    }, [tasks, sortBy, sortOrder]);
+
     return (<>
         <h2 className="task-list-title">Lista delle task:</h2>
         <div>
             <div className="header-table">
-                <div><strong>Nome</strong></div>
-                <div><strong>Stato</strong></div>
-                <div><strong>Data di creazione</strong></div>
+                <div onClick={() => handleSort('title')} style={{ cursor: 'pointer' }}><strong>Nome</strong></div>
+                <div onClick={() => handleSort('status')} style={{ cursor: 'pointer' }}><strong>Stato</strong></div>
+                <div onClick={() => handleSort('createdAt')} style={{ cursor: 'pointer' }}><strong>Data di creazione</strong></div>
             </div>
             <div className="body-table">
-                {tasks && tasks.map(t =>
+                {sortedTasks.map(t =>
                     <TaskRow key={t.id} task={t} />
                 )}
             </div>
